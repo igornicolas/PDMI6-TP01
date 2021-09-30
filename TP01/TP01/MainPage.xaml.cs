71efa11849af66125e73810fafdd659469e28d32
igornicolas <igornicolas33@gmail.com>
2021-09-30 17:21:55 -0300

    full project

---
+++ TP01/TP01/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TP01
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void okClick(object sender, EventArgs e)
+        {
+            string idValue = id.Text;
+            string passwordValue = password.Text;
+            if (idValue == "admin" && passwordValue == "senha@dmin")
+            {
+                await DisplayAlert("Login", "Login realizado com sucesso!", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Login", "Login não autorizado", "OK");
+            }
+        }
+
+        private void limparClick(object sender, EventArgs e)
+        {
+            id.Text = "";
+            password.Text = "";
+
+        }
+
+        async void creditosClick(object sender, EventArgs args)
+        {
+            await DisplayAlert("Autores", "Lorrane Meneses dos Santos e Igor Nicolas", "OK");
+        }
+    }
+}
